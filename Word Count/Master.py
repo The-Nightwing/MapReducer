93fbee037c62fb8bb9f2134873cc9f4a494be305
The-Nightwing <vermashivam0606@gmail.com>
2023-04-27 00:16:40 +0530

    changes

--- Word Count/Master.py
+++ Word Count/Master.py
@@ -8,6 +8,7 @@ import multiprocessing
 import threading
 import os
 
+
 class Master(Master_pb2_grpc.MasterServicer):
     def __init__(self, inputLocation, outputLocation, M, R):
         super().__init__()
@@ -16,34 +17,46 @@ class Master(Master_pb2_grpc.MasterServicer):
         self.M = M
         self.R = R
 
-        # start M processes and open mapper.py 
+        # start M processes and open mapper.py
         # start R processes and open reducer.py
         self.spawnMappers()
         self.spawnReducers()
 
     def mapFilesToMappers(self):
         files = os.listdir(self.inputLocation)
-        self.filesPerMapper = [files[i::self.M] for i in range(self.M)]
-        
-    
+        self.filesPerMapper = [[] for i in range(self.M)]
+        for i in range(len(files)):
+            self.filesPerMapper[i % self.M].append(files[i])
+
     def worker(self, request, context):
         self.mapFilesToMappers()
+        threads = []
+
         for i in range(self.M):
             thread = threading.Thread(target=self.startMapping(i))
             thread.start()
-            
+            threads.append(thread)
+
+    def startReduce(self):
+        for i in range(self.R):
+            with grpc.insecure_channel('localhost:'+str(6000+self.M+i)) as channel:
+                stub = mapper_pb2_grpc.MapperStub(channel)
+                response = stub.reduce(mapper_pb2.MapperRequest())
+        
     def startMapping(self, index):
         with grpc.insecure_channel('localhost:'+str(6000+index)) as channel:
             stub = mapper_pb2_grpc.MapperStub(channel)
-            response = stub.map(mapper_pb2.MapperRequest(reducers = self.R, filenames = self.filesPerMapper[0]))
-            print(response.status)
+            response = stub.map(mapper_pb2.MapperRequest(
+                reducers=self.R, filenames=self.filesPerMapper[index]))
+        
 
     def spawnReducers(self):
         self.spawnReducers = []
         for i in range(self.R):
             name = i+1
             port = 6000+self.M+i
-            process = multiprocessing.Process(target=os.system, args=(f'python reducer.py {name} {port}'))
+            process = multiprocessing.Process(
+                target=os.system, args=(f'python reducer.py {name} {port}'))
             self.spawnReducers.append(process)
             process.start()
 
@@ -52,32 +65,34 @@ class Master(Master_pb2_grpc.MasterServicer):
         for i in range(self.M):
             name = i+1
             port = 6000+i
-            process = multiprocessing.Process(target=os.system, args=(f'python mapper.py {name} {port}'))
+            process = multiprocessing.Process(
+                target=os.system, args=(f'python mapper.py {name} {port}'))
             self.spawnedMappers.append(process)
             process.start()
-    
+
     def mapperFinished(self, request, context):
-        print(request.status)
+        self.mapperFinishCounter+=1
+        if self.mapperFinishCounter == self.M:
+            self.startReduce()
 
-    def reducerFinished(self, request, context):
-        print(request.status)
+import sys
 
-
-def start():
-    inputLocation = input("Input Location: ")
-    outputLocation = input("Output Location: ")
-    M = int(input("Number of Mappers: "))
-    R = int(input("Number of Reducers: "))
-
+def start(inputLocation, outputLocation, M, R):
     port = '8888'
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    Master_pb2_grpc.add_MasterServiceServicer_to_server(Master(inputLocation, outputLocation, M, R), server)
+    Master_pb2_grpc.add_MasterServiceServicer_to_server(
+        Master(inputLocation, outputLocation, M, R), server)
     server.add_insecure_port('[::]:' + port)
     server.start()
     server.wait_for_termination()
-    
+
+
 if __name__ == "__main__":
-    start()
+    inputLocation = sys.argv[1]
+    outputLocation = sys.argv[2]
+    M = int(sys.argv[3])
+    R = int(sys.argv[4])
+    start(inputLocation, outputLocation, M, R)
 
 
 # python -m grpc_tools.protoc -I./  --python_out=ProtoFiles/ ./Master.proto --pyi_out=ProtoFiles/ --grpc_python_out=ProtoFiles/
--- Word Count/mapper.py
+++ Word Count/mapper.py
@@ -3,8 +3,8 @@ from concurrent import futures
 import grpc
 import ProtoFiles.mapper_pb2 as mapper_pb2
 import ProtoFiles.mapper_pb2_grpc as mapper_pb2_grpc
-import ProtoFiles.Master_pb2 as master_pb2
-import ProtoFiles.Master_pb2_grpc as master_pb2_grpc
+import ProtoFiles.master_pb2 as master_pb2
+import ProtoFiles.master_pb2_grpc as master_pb2_grpc
 
 
 class Mapper(mapper_pb2_grpc.ServerServicer):
@@ -26,20 +26,25 @@ class Mapper(mapper_pb2_grpc.ServerServicer):
                         keyVPairs.append((word, 1))
 
         self.partitionStrategy(keyVPairs)
-
-        return master_pb2.MapperResponse(status='Mapper Finished')
-
-    def partitonStrategy(self, keyVPairs):
+        self.notifyMaster()
+    
+    def notifyMaster(self):
+        with grpc.insecure('localhost:8888') as channel:
+            stub = master_pb2_grpc.MasterServiceStub(channel)
+            response = stub.mapperFinished(master_pb2.MapperFinishedRequest())
+            print('Mapper Done')
+            
+    def partitionStrategy(self, keyVPairs):
         for tuple in keyVPairs:
             hash = int(hash(tuple[0]))
             reducer = hash % self.reducers
             with open('reducer_'+reducer+'.txt', 'a') as f:
                 f.write(tuple[0] + ' ' + tuple[1] + '\n')
 
-
 def main(port):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    mapper_pb2_grpc.add_RegistryServerServicer_to_server(Mapper(name, port), server)
+    mapper_pb2_grpc.add_RegistryServerServicer_to_server(
+        Mapper(name, port), server)
     server.add_insecure_port('[::]:' + port)
     server.start()
     print("Server started, listening on " + port)
